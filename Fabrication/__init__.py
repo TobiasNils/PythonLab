393851024c60d5cf5c27b331b7d17c8124a8a8a0
tobiasnils <mail@tnils.eu>
2019-03-04 11:59:36 +0100

    Moving Fabrication-related scripts to Desktop branch. [Ticket: X]

--- Fabrication/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Allow python to import packes from the files defined in "/lib"
-"""
-
--- Measurement/modules/MyStage.py
+++ Measurement/modules/MyStage.py
@@ -53,13 +53,14 @@ class MyStage(object):
     TIMEOUT = 0.05
     DEBUG = False
     serialPort = ''
-        
+    time = __import__('time')
+    #pos = [0,0,0]        
     def __init__(self, os, serial_module, n_xips=1):
         self.os = os
         self.sl = serial_module        
         self.n_xips = n_xips
         self.positions_set = [False for pos in range(self.n_xips)]
-        connection = None
+        self.connection = None
         
         print('\n Stage module loaded - do "dmesg | grep tty" to list Ports of connected serial devices:\n')
         import subprocess
@@ -91,7 +92,11 @@ class MyStage(object):
             print('\n Try to connect to SerialPort: \n' + self.serialPort)
             self.connect(self.serialPort)
         #print("\n Command exit status/return code : ", p_status)
-            #print("\n In case MyStage.connect() returns error: Access denied, check that the correct serialPort was used and launch MyStage.get_permission(serialPort) again.")
+            #print("\n In case MyStage.connect() returns error: Access denied, 
+            #check that the correct serialPort was used and launch 
+            #MyStage.get_permission(serialPort) again.")
+            
+            
             #self.user_init()
             
         except self.sl.serialutil.SerialException as se:
@@ -134,36 +139,54 @@ class MyStage(object):
         #Stage.write(b'0_0_0_setpos_') # sets Origin at current position
     def set_home(self):
         # the next line initializes the internal step tracking of MyAxis
-        self.positions_set[0] = not self.positions_set[0]
-        print('home_set is set to ' + str(self.positions_set[0]))
+#        self.positions_set[0] = not self.positions_set[0]
+#        print('home set and saved to Corvus Memory')
         # ...the ordinary set home command in Venus1 language:
         self.write(b'0_0_0_setpos_')
-
-
-    def set_positions(self):
-        self.positions_set = [not pos for pos in self.positions_set]
+        self.save()
         
-        print('All positions set. Monitoring can be started. ')
-    
         
+    def go_home(self):
+        self.write(b'0_0_0_m_')
+        self.wait_for_movement()
+        response = self.get_pos()
+        print( 'new position:\n' + str(response))
+
     def get_pos(self):
-        self.write(b'pos_')    
-        self.read()
+        response = self.send(b'pos_')[:-2]
+        pos = [float(val) for val in response.split()]
+        return pos
+    
+    def set_velocity(self, velocity):
+        self.send(b'%f_setvel_' %(velocity))
+        print('Maximum Velocity set to %f.'%(velocity))
+    
+    def moving(self):
+        moving = False
+        for i in range(3):
+            if self.send(b'%i_nst_'% (i+1))[:1] == str(0):
+                pass
+            else:
+                moving = True
+        return moving
+    
+    def wait_for_movement(self):
+        while True:
+            moving = self.moving()
+            if moving:
+                self.time.sleep(.05)
+                pass
+            else:
+                break
                     
     def save(self):
         self.write(b'save_')
         # saves settings on Corvus memory for next session
 
-    def send(self, command, numEnquiries = 1):
+    def send(self, command):
         self.connection.flushInput()
         self.write(command)
-        #if mnemonic != C['ETX']: self.read()
-        #self.read()
-        #self.getACQorNAK() # try with this line commented
-        response = []
-        for i in range(numEnquiries):
-            #self.enquire()
-            response.append(self.read())
+        response = self.read().decode('utf-8')
         return response
 
     def write(self,what):
@@ -171,174 +194,193 @@ class MyStage(object):
         self.connection.write(what)
         
     def read(self):
-        response = self.connection.read()
+        response = self.connection.readall()
         return response
+    
     def close(self):
         #self.debugMessage(what)
-        self.connection.close()
-        
+        self.connection.close()     
 
-class MyAxis(object):
+
+         
+class MyAxes(object):
     
+    #np = __import__('numpy')
     
-    #sidestep = [0,0,0]
-    
-        
-    
-    def __init__(self, axis_number, parent):
-        #self.name = 'Axis' + axis_number        
-        self.axis_number = str(axis_number)
-        self.parent = parent
-        self.step_down = [0 for n in range(self.parent.n_xips)]
-        self.step_out = [0 for n in range(self.parent.n_xips)]
-        self.step_in = [0 for n in range(self.parent.n_xips)]
+    def __init__(self, stage, x=False, y=False, z=False):
+        #self.name = 'Axis' + axis_number  
+        if not x and not y and not z:
+            raise AttributeError(
+                    'Please define at least one dimension. ')    
+        else:
+            self.parent = stage
+            self.xyz = (x,y,z)            
 
     def send(self, command):
         #command = command.replace('AX', self.axis_number)
         return self.parent.send(command)
 
-    #def actual_position(self):
-        #return self.send(b'AX_np_')
-
-    #def homing(self):
-        ### homing (search limit reverse)
-        #return self.send('AX_ncal_')
-
-    def move_relative(self, by):
+    def move_relative(self, *xyz):
+        moves = [0,0,0]
+        i=0
+        for arg in xyz:
+            while True:
+                if i>2:
+                    break
+                elif not self.xyz[i]:
+                    i+=1
+                else:
+                    moves[i]=arg
+                    i+=1
+                    break
+                    
+        self.send(b'%f_%f_%f_r_' % (moves[0], moves[1], moves[2]))  
+        self.parent.wait_for_movement()
+        response = self.parent.get_pos()
+        print( 'new position:\n' + str(response))
         
-        if self.axis_number == '1':
-            self.send(b'%f_0_0_r_' % (by))
-        elif self.axis_number == '2':
-            self.send(b'0_%f_0_r_' % (by))
-        elif self.axis_number == '3':
-            self.send(b'0_0_%f_r_' % (by))            
-        else:
-            print('Error: Axis numer out of range.')
-   
-    # Next we define specific movements with the aim of creating an internal 
+    def move_global(self, *xyz):
+        moves = self.parent.get_pos()
+        i=0
+        for arg in xyz:
+            while True:
+                if i>2:
+                    break
+                elif not self.xyz[i]:
+                    i+=1
+                else:
+                    moves[i]=arg
+                    i+=1
+                    break
+                    
+        self.send(b'%f_%f_%f_m_' % (moves[0], moves[1], moves[2]))  
+        self.parent.wait_for_movement()
+        response = self.parent.get_pos()
+        print( 'new position:\n' + str(response))
+        # Next we define specific movements with the aim of creating an internal 
     # (class owned) memory for the movements performed after the boolean 
     # variables "home_set" and "step1_set" respectively have been set to "True".
     # The steps thus defined can be called upon afterwards from by the 
     # monitoring cycle to carry out the correct movements at each step.
 
-    def move_out(self, by):
-        
-        #if activated by MyStage.set_home(), the next lines create an internal 
-        # step tracking
-        if self.parent.positions_set[0] == True and not self.parent.positions_set[1] == True:
-            if self.axis_number == '2':
-                self.step_in[0] = self.step_in[0] - by
-                self.send(b'0_-%f_0_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement')
-
-        elif self.parent.positions_set[0] == True and self.parent.positions_set[1] == True and not self.parent.positions_set[2] == True:
-            if self.axis_number == '2':
-                self.step_in[1] = self.step_in[1] - by
-                self.send(b'0_-%f_0_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement')
-                
-        elif self.parent.positions_set[0] == True and self.parent.positions_set[1]== True and self.parent.positions_set[2] == True:
-            if self.axis_number == '2':
-                self.step_in[2] = self.step_in[2] - by
-                self.send(b'0_-%f_0_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement')
-                
-        # if step tracking is not needed, the ordinary command is executed:
-        else:
-            if self.axis_number == '2':
-                self.send(b'0_-%f_0_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement')
-
-    def move_up(self, by):
-
-        if self.parent.positions_set[0] == True and self.parent.positions_set[1:] == [False for value in self.parent.positions_set[1:]]:
-            if self.axis_number == '3':
-                self.step_down[0] = self.step_down[0] - by
-                self.send(b'0_0_-%f_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement.')
-
-        elif self.parent.positions_set[:1] == [True for value in self.parent.positions_set[:1]] and self.parent.positions_set[2:] == [False for value in self.parent.positions_set[2:]]:
-            if self.axis_number == '3':
-                self.step_down[1] = self.step_down[1] - by
-                self.send(b'0_0_-%f_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement.')
-        elif self.parent.positions_set[:2] == [True for value in self.parent.positions_set[:2]] and self.parent.positions_set[3:] == [False for value in self.parent.positions_set[3:]]:
-            if self.axis_number == '3':
-                self.step_down[2] = self.step_down[2] - by
-                self.send(b'0_0_-%f_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement.')
-
-        else: 
-            if self.axis_number == '3':
-                self.send(b'0_0_-%f_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement.')            
-            
-            
-    def move_down(self, by):
-        
-        if self.parent.positions_set[0] == True and self.parent.positions_set[1:] == [False for value in self.parent.positions_set[1:]]:
-            if self.axis_number == '3':
-                self.step_down[0] = self.step_down[0] + by
-                self.send(b'0_0_%f_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement.')
-
-        elif self.parent.positions_set[:1] == [True for value in self.parent.positions_set[:1]] and self.parent.positions_set[2:] == [False for value in self.parent.positions_set[2:]]:
-            if self.axis_number == '3':
-                self.step_down[1] = self.step_down[1] + by
-                self.send(b'0_0_%f_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement.')
-        elif self.parent.positions_set[:2] == [True for value in self.parent.positions_set[:2]] and self.parent.positions_set[3:] == [False for value in self.parent.positions_set[3:]]:
-            if self.axis_number == '3':
-                self.step_down[2] = self.step_down[2] + by
-                self.send(b'0_0_%f_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement.')
-
-        else:
-            if self.axis_number == '3':
-                self.send(b'0_0_%f_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement')
-            
-    def move_in(self, by):
-
-        if self.parent.positions_set[0] == True and not self.parent.positions_set[1] == True:
-            if self.axis_number == '2':
-                self.step_in[0] = self.step_in[0] + by
-                self.send(b'0_%f_0_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement')
-        
-        elif self.parent.positions_set[0] == True and self.parent.positions_set[1] == True and not self.parent.positions_set[2] == True:
-            if self.axis_number == '2':
-                self.step_in[1] = self.step_in[1] + by
-                self.send(b'0_%f_0_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement')
-                
-        elif self.parent.positions_set[0] == True and self.parent.positions_set[1] == True and self.parent.positions_set[2] == True:
-            if self.axis_number == '2':
-                self.step_in[2] = self.step_in[2] + by
-                self.send(b'0_%f_0_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement')
-                
-        else:
-            if self.axis_number == '2':
-                self.send(b'0_%f_0_r_' % (by))
-            else:
-                print('Error: Axis number not defined for that movement')
-                
+#    def move_out(self, by):
+#        
+#        #if activated by MyStage.set_home(), the next lines create an internal 
+#        # step tracking
+#        if self.parent.positions_set[0] == True and not self.parent.positions_set[1] == True:
+#            if self.axis_number == '2':
+#                self.step_in[0] = self.step_in[0] - by
+#                self.send(b'0_-%f_0_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement')
+#
+#        elif self.parent.positions_set[0] == True and self.parent.positions_set[1] == True and not self.parent.positions_set[2] == True:
+#            if self.axis_number == '2':
+#                self.step_in[1] = self.step_in[1] - by
+#                self.send(b'0_-%f_0_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement')
+#                
+#        elif self.parent.positions_set[0] == True and self.parent.positions_set[1]== True and self.parent.positions_set[2] == True:
+#            if self.axis_number == '2':
+#                self.step_in[2] = self.step_in[2] - by
+#                self.send(b'0_-%f_0_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement')
+#                
+#        # if step tracking is not needed, the ordinary command is executed:
+#        else:
+#            if self.axis_number == '2':
+#                self.send(b'0_-%f_0_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement')
+#
+#    def move_up(self, by):
+#
+#        if self.parent.positions_set[0] == True and self.parent.positions_set[1:] == [False for value in self.parent.positions_set[1:]]:
+#            if self.axis_number == '3':
+#                self.step_down[0] = self.step_down[0] - by
+#                self.send(b'0_0_-%f_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement.')
+#
+#        elif self.parent.positions_set[:1] == [True for value in self.parent.positions_set[:1]] and self.parent.positions_set[2:] == [False for value in self.parent.positions_set[2:]]:
+#            if self.axis_number == '3':
+#                self.step_down[1] = self.step_down[1] - by
+#                self.send(b'0_0_-%f_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement.')
+#        elif self.parent.positions_set[:2] == [True for value in self.parent.positions_set[:2]] and self.parent.positions_set[3:] == [False for value in self.parent.positions_set[3:]]:
+#            if self.axis_number == '3':
+#                self.step_down[2] = self.step_down[2] - by
+#                self.send(b'0_0_-%f_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement.')
+#
+#        else: 
+#            if self.axis_number == '3':
+#                self.send(b'0_0_-%f_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement.')            
+#            
+#            
+#    def move_down(self, by):
+#        
+#        if self.parent.positions_set[0] == True and self.parent.positions_set[1:] == [False for value in self.parent.positions_set[1:]]:
+#            if self.axis_number == '3':
+#                self.step_down[0] = self.step_down[0] + by
+#                self.send(b'0_0_%f_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement.')
+#
+#        elif self.parent.positions_set[:1] == [True for value in self.parent.positions_set[:1]] and self.parent.positions_set[2:] == [False for value in self.parent.positions_set[2:]]:
+#            if self.axis_number == '3':
+#                self.step_down[1] = self.step_down[1] + by
+#                self.send(b'0_0_%f_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement.')
+#        elif self.parent.positions_set[:2] == [True for value in self.parent.positions_set[:2]] and self.parent.positions_set[3:] == [False for value in self.parent.positions_set[3:]]:
+#            if self.axis_number == '3':
+#                self.step_down[2] = self.step_down[2] + by
+#                self.send(b'0_0_%f_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement.')
+#
+#        else:
+#            if self.axis_number == '3':
+#                self.send(b'0_0_%f_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement')
+#            
+#    def move_in(self, by):
+#
+#        if self.parent.positions_set[0] == True and not self.parent.positions_set[1] == True:
+#            if self.axis_number == '2':
+#                self.step_in[0] = self.step_in[0] + by
+#                self.send(b'0_%f_0_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement')
+#        
+#        elif self.parent.positions_set[0] == True and self.parent.positions_set[1] == True and not self.parent.positions_set[2] == True:
+#            if self.axis_number == '2':
+#                self.step_in[1] = self.step_in[1] + by
+#                self.send(b'0_%f_0_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement')
+#                
+#        elif self.parent.positions_set[0] == True and self.parent.positions_set[1] == True and self.parent.positions_set[2] == True:
+#            if self.axis_number == '2':
+#                self.step_in[2] = self.step_in[2] + by
+#                self.send(b'0_%f_0_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement')
+#                
+#        else:
+#            if self.axis_number == '2':
+#                self.send(b'0_%f_0_r_' % (by))
+#            else:
+#                print('Error: Axis number not defined for that movement')
+#                
 
         
     """        
